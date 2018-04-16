bedf6f1e22d071abe02b005b5576d8d65d6d9b5d
mrwagner27 <36823560+mrwagner27@users.noreply.github.com>
2018-04-15 18:57:20 -0700

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+
+
+module.exports.run = async (bot, message, args) => {
+
+
+
+message.reply(`pong! \`${Math.floor(bot.ping)}ms\``)
+
+
+
+ }
+
+
+
+module.exports.help = {
+
+    name: "ping"
+
+}
